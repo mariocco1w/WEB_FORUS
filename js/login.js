71845a3fa8cd6c231c4c3f537e24fb24accd469e
mariocco1w <mariorenatomonroyc@gmail.com>
2026-02-03 10:09:05 -0600

    arreglo iniciacion home

--- js/login.js
+++ js/login.js
@@ -3,19 +3,21 @@ const input = document.getElementById("alias");
 const button = document.getElementById("login-btn");
 const message = document.getElementById("login-message");
 
-button.addEventListener("click",() => {
-    const valor = input.vallue.trim();
-
-    if(valor === aliasCorrecto){
-        window.location.href= "home.html";
-    }else{
-        message.textContent ="ACCESO DENEGADO, ESTE LUGAR ES PARA ALGUIEN ESPECIAL";
-        input.vallue="";
-    }
-});
+button.addEventListener("click", intentarLogin);
 
 input.addEventListener("Keydown", e => {
     if(e.key === "Enter"){
         button.click();
     }
 });
+
+function intentarLogin(){
+    const valor = input.value.trim().toLowerCase();
+    if(valor === aliasCorrecto.toLowerCase()){
+        console.log("login correcto, entrando ... ");
+    }else{
+        console.log("Alias incorrecto",valor);
+        message.textContent("Este sitio no esta echo para cualquier persona ... ");
+        input.value = "";
+    }
+}
