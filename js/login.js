db6a10801e1427e8718f7706e9abde195ac5e7c0
mariocco1w <mariorenatomonroyc@gmail.com>
2026-02-03 10:45:43 -0600

    complemento para redireccion

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ const button = document.getElementById("login-btn");
 const message = document.getElementById("login-message");
 
 button.addEventListener("click", intentarLogin);
-
+ 
 input.addEventListener("Keydown", e => {
     if(e.key === "Enter"){
         button.click();
@@ -15,6 +15,7 @@ function intentarLogin(){
     const valor = input.value.trim().toLowerCase();
     if(valor === aliasCorrecto.toLowerCase()){
         console.log("login correcto, entrando ... ");
+        window.location.assign("home.html");
     }else{
         console.log("Alias incorrecto",valor);
         message.textContent("Este sitio no esta echo para cualquier persona ... ");
