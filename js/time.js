0195d99c600a9e230d081160f1144a7057e30dbc
mariocco1w <mariorenatomonroyc@gmail.com>
2026-02-03 22:53:31 -0600

    cambios varios

--- js/time.js
+++ js/time.js
@@ -1,18 +1,20 @@
 const startDate = new Date("2025-09-03T17:39:00");
-const counter = document.getElementById("time-counter");
-
-function updateTime(){
+function updateCounter() {
     const now = new Date();
-    const diff =  now - startDate;
-
-    const days = Math.floor(diff / (1000*60*60*24));
-    const hours = Math.floor((diff / (100*60*60))%24);
-    const minute = Math.floor((diff /(1000*60))%60);
-    const seconds = Math.floor((diff / 1000)%60);
-
-    counter.textContent=
-    '${days} días · ${hours} horas · ${minutes} minutos · ${seconds} segundos';
-}
-
-setInterval(updateTime, 1000);
-updateTime();
+    let diff = Math.floor((now - startDate) / 1000);
+  
+    const days = Math.floor(diff / (3600 * 24));
+    diff %= 3600 * 24;
+  
+    const hours = Math.floor(diff / 3600);
+    diff %= 3600;
+  
+    const minutes = Math.floor(diff / 60);
+    const seconds = diff % 60;
+  
+    document.getElementById("time-counter").textContent =
+      `${days} días · ${hours} horas · ${minutes} minutos · ${seconds} segundos`;
+  }
+  
+  updateCounter();
+  setInterval(updateCounter, 1000);
